245b2c5d66d08add45eec86c06492fe37f71e6f7
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2021-02-18 17:30:54 +0100

    Implement timed step function on pitch

--- src/modules/fw_att_control/SystemIdController.cpp
+++ src/modules/fw_att_control/SystemIdController.cpp
@@ -3,7 +3,54 @@
 SystemIdController::SystemIdController() :
 	ModuleParams(nullptr)
 {
+	_init_time = hrt_absolute_time();
+	PX4_INFO("%d Sysid initialized", (int)_init_time);
+}
 
-	_start_time = hrt_absolute_time();
-	PX4_INFO("SysId start time: %d\n", (int)_start_time);
+void SystemIdController::sysid_activate()
+{
+	_is_active = true;
+	set_sysid_start_time();
+	PX4_INFO("%d Sysid maneuver starting", (int)_sysid_start_time);
+}
+
+void SystemIdController::sysid_deactivate()
+{
+	_is_active = false;
+	_should_run = false;
+	PX4_INFO("Sysid maneuver finished");
+}
+
+void SystemIdController::update()
+{
+	// Activate after a certain time
+	if (!_is_active && _should_run && time_since_init() > _delay_before_start)
+	{
+		sysid_activate();
+	}
+
+	if (_is_active)
+	{
+		_input = generate_signal_step();
+		//PX4_INFO("Input: %f", (double)_input);
+
+		if (time_elapsed() > _sysid_duration)
+		{
+			sysid_deactivate();
+		}
+	}
+
+}
+
+float SystemIdController::generate_signal_step()
+{
+	if (time_elapsed() < _step_length)
+	{
+		return 0;
+	}
+	else if (time_elapsed() < _step_length * 2)
+	{
+		return _input_high;
+	}
+	return 0;
 }
--- src/modules/fw_att_control/SystemIdController.h
+++ src/modules/fw_att_control/SystemIdController.h
@@ -14,17 +14,31 @@ public:
 	SystemIdController();
 	~SystemIdController() = default;
 
-	void activate() {_is_active = true;}
-	void deactivate() {_is_active = false;}
 	bool is_active() {return _is_active;}
+	float get_input(){return _input;}
 
 	void update();
-
 private:
+	bool _should_run = true;
+	bool _is_active = false; // will generate a step on roll
+	int _init_time;
+	int _sysid_start_time;
 
-	bool _is_active = false;
-	uint64_t _start_time;
-	uint64_t _delay_before_start = 1000000; // TODO change
+	int _delay_before_start = 50 * 1000000; // TODO change
+	float _input_low = 0.2;
+	float _input_high = 0.4;
+	int _step_length = 1* 1000000; // us
+	int _sysid_duration = _step_length * 2;
+
+	float _input; // Number between -1 and 1
+
+	void sysid_activate();
+	void sysid_deactivate();
+
+	int time_since_init(){return hrt_absolute_time() - _init_time;}
+	int time_elapsed(){return hrt_absolute_time() - _sysid_start_time;}
+	void set_sysid_start_time(){_sysid_start_time = hrt_absolute_time();}
+	float generate_signal_step();
 
 	//DEFINE_PARAMETERS( // TODO add parameters
 	//	(ParamBool<px4::params::WV_EN>) _param_wv_en,
